d5be79c16cd930a67c66aa96b3eeeb32e48c7763
Ciro836 <cirschot@gmail.com>
2025-10-20 16:38:39 -0300

    tercer commit con github desktop

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 void main()
 {
     System.out.println("Holaaa, este es mi segundo commit");
-    for (int i = 0; i <= 10; i++)
-    {
-        System.out.println(i);
-    }
+
+    System.out.println("Tercer commit, con github desktop");
 }
