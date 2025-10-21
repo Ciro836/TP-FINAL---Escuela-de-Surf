fbe059b0e245c8919d9918798ba99494849b39ec
zoearoza <zoearoza@gmail.com>
2025-10-21 18:35:05 -0300

    Atributos y constructores de clase persona

--- src/Clases/Persona.java
+++ src/Clases/Persona.java
@@ -1,6 +1,28 @@
 package Clases;
 
-public abstract class Persona
-{
+public abstract class Persona{
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String numeroTel;
+
+    /// CONSTRUCTORES
+
+    public Persona(){
+        this.dni = "";
+        this.nombre = "";
+        this.apellido = "";
+        this.edad = 0;
+        this.numeroTel = "";
+    }
+
+    public Persona(String dni, String nombre, String apellido, int edad, String numeroTel) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.numeroTel = numeroTel;
+    }
 
 }
