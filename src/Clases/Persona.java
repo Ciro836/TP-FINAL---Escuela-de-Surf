c0b54a27ad9b32af42801436ac73d9f753a3250a
zoearoza <zoearoza@gmail.com>
2025-10-21 18:35:17 -0300

    Update Persona.java

--- src/Clases/Persona.java
+++ src/Clases/Persona.java
@@ -25,4 +25,53 @@ public abstract class Persona{
         this.numeroTel = numeroTel;
     }
 
+    /// MOSTRAR UNA PERSONA
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", edad=" + edad +
+                ", numeroTel='" + numeroTel + '\'' +
+                '}';
+    }
+
+    /// GETTERS Y SETTERS
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNumeroTel() {
+        return numeroTel;
+    }
+
+    public void setNumeroTel(String numeroTel) {
+        this.numeroTel = numeroTel;
+    }
 }
