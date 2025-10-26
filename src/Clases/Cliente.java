1e8c173248200965a1187cc277be0edb290a257f
Ciro Schot <cirschot@gmail.com>
2025-10-26 06:23:32 -0300

    Creación de la clase Cliente y modificaciones en Alumno
    #Nueva clase cliente con id autoincrementable.
    #Modificación de alumno: le agregue el id autoincrementable y elimine que en los constructores se pasa por parametro un ArrrayList, directamente ya se crea dentro del constructor.

--- src/Clases/Cliente.java
+++ src/Clases/Cliente.java
@@ -2,9 +2,61 @@ package Clases;
 
 import Interfaces.Pagos;
 
-public class Cliente implements Pagos
-{
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cliente extends Persona implements Pagos
+{
+    private static int contador = 0;
+    private int idCliente;
+    private final List<Pago> pagos;
+    private final List<Alquiler> alquileres;
+
+    /// CONSTRUCTORES
+
+    public Cliente()
+    {
+        this.idCliente = ++contador;
+        this.pagos = new ArrayList<>();
+        this.alquileres = new ArrayList<>();
+    }
+
+    public Cliente(String dni, String nombre, String apellido, int edad, String numeroTel)
+    {
+        super(dni, nombre, apellido, edad, numeroTel);
+        this.idCliente = ++contador;
+        this.pagos = new ArrayList<>();
+        this.alquileres = new ArrayList<>();
+    }
+
+    /// GETTERS Y SETTERS
+
+    public static int getContador()
+    {
+        return contador;
+    }
+
+    public int getIdCliente()
+    {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente)
+    {
+        this.idCliente = idCliente;
+    }
+
+    public List<Pago> getPagos()
+    {
+        return pagos;
+    }
+
+    public List<Alquiler> getAlquileres()
+    {
+        return alquileres;
+    }
+
+    /// METODOS
 
     @Override
     public boolean esMoroso()
