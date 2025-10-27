61c8328cd22c3c1a2bb43e3e9970a36135addbd0
zoearoza <zoearoza@gmail.com>
2025-10-26 23:22:23 -0300

    Prueba en main del repositorio

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,28 @@ void main()
 
         repositorioAlumno.agregar(1, new Alumno("43851970", "Zoe","Arozamena ", 23 ,"2234496290",NivelDeSurf.PRINCIPIANTE, 5));
 
-        repositorioAlumno.mostrarTodos();
+        escuela.getRepoAlumnos().mostrarTodos();
+
+        Alumno alumno = escuela.getRepoAlumnos().buscarPorId(1);
+        if (alumno != null) {
+            System.out.println("Alumno: " + alumno);
+        }else{
+            System.out.println("No existe alumno");
+        }
+
+        Instructor instructorNuevo = new Instructor("24734142", "Soledad", "Arias", 50, "22346161847", 15, 600000);
+
+        ClaseDeSurf clase = new ClaseDeSurf(instructorNuevo, TipoClase.PARTICULAR, LocalDateTime.of(2025,10,27,16,30), 1);
+
+        Pago pagoNuevo = new Pago();
+
+        Reserva reservaNueva = new Reserva(alumnoNuevo, clase, pagoNuevo);
+
+        escuela.getRepoAlumnos().mostrarTodos();
+
+        escuela.getRepoClases().agregar(1, clase);
+        escuela.getRepoClases().mostrarTodos();
+        
 
 
 
