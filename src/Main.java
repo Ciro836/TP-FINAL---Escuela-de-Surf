3bfa155174bc6154527547b8acbcb0f0d4375773
zoearoza <zoearoza@gmail.com>
2025-10-26 22:57:29 -0300

    Probando repositorio...

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 void main()
 {
+    public static void main (String[] args)
+    {
+        //probando...
+        
+        Repositorio <Alumno> repositorioAlumno = new Repositorio<>();
+
+        repositorioAlumno.agregar(1, new Alumno("43851970", "Zoe","Arozamena ", 23 ,"2234496290",NivelDeSurf.PRINCIPIANTE, 5));
+
+        repositorioAlumno.mostrarTodos();
+
+
+
+
+    }
+
+
 
 }
