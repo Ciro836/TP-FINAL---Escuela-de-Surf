42dc16e8955f5cd69b4a13ac8e29e8ecb4bd3585
zoearoza <zoearoza@gmail.com>
2025-11-08 14:24:38 -0300

    Agregué metodos para leer el archivo e instanciar el objeto
    Agregue el metodo JsonArray_a_Repositorio, que lo que hace es recorrer el jsonarray, y declara una nueva instancia de la clase correspondiente, simepre y cuando implemente la interfaceJson. Luego guarda el objeto armado del fromJson, y lo agrega en el repositorio
    Y desp está el metodo leerRepositorioDesdeJson, donde paso los repositorios y el arhcivo a leer, creo un string builder para leer cada línea del archivo y guardarla. Y por ultimo llamo al método para armar el repositorio, con el if compruebo que el jObject contenga el repositorio

--- src/Clases/JsonUtiles.java
+++ src/Clases/JsonUtiles.java
@@ -6,10 +6,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 
 public class JsonUtiles
@@ -89,4 +88,75 @@ public class JsonUtiles
         }
         return jsonArray;
     }
+
+    public static void leerRepositorioDesdeJson(Repositorio<Alumno> repoAlumno, Repositorio<Instructor> repoInstructor,
+                                                Repositorio<ClaseDeSurf> repoClase, Repositorio<Cliente> repoCliente,
+                                                Repositorio<Reserva> repoReserva, Repositorio<Equipo> repoEquipo,
+                                                Repositorio<Alquiler> repoAlquiler,
+                                                Repositorio<Pago> repoPago, String archivo){
+        try{
+            StringBuilder contenidoJson = new StringBuilder();
+            try(BufferedReader lector = new BufferedReader(new FileReader(archivo))){
+                String linea;
+                while((linea = lector.readLine()) != null){
+                    contenidoJson.append(linea);
+                }
+            }
+
+            JSONObject jObject = new JSONObject(contenidoJson.toString());
+
+            if (jObject.has("repoAlumnos"))
+                JsonArray_a_Repositorio(jObject.getJSONArray("repoAlumnos"), repoAlumno, Alumno.class);
+            if (jObject.has("repoInstructores"))
+                JsonArray_a_Repositorio(jObject.getJSONArray("repoInstructores"), repoInstructor, Instructor.class);
+            if (jObject.has("repoClases"))
+                JsonArray_a_Repositorio(jObject.getJSONArray("repoClases"), repoClase, ClaseDeSurf.class);
+            if (jObject.has("repoClientes"))
+                JsonArray_a_Repositorio(jObject.getJSONArray("repoClientes"), repoCliente, Cliente.class);
+            if (jObject.has("repoReservas"))
+                JsonArray_a_Repositorio(jObject.getJSONArray("repoReservas"), repoReserva, Reserva.class);
+            if (jObject.has("repoEquipos"))
+                JsonArray_a_Repositorio(jObject.getJSONArray("repoEquipos"), repoEquipo, Equipo.class);
+            if (jObject.has("repoAlquileres"))
+                JsonArray_a_Repositorio(jObject.getJSONArray("repoAlquileres"), repoAlquiler, Alquiler.class);
+            if (jObject.has("repoPagos"))
+                JsonArray_a_Repositorio(jObject.getJSONArray("repoPagos"), repoPago, Pago.class);
+
+            System.out.println("Repositorios cargados correcamente desde " + archivo);
+        }catch (Exception e){
+            System.out.println("Error al leer los datos del archivo" + e.getMessage());
+        }
+    }
+
+
+    // paso por paramentro class<t> clase, para luego poder hacer clase.get para que sepa que tipo de instancias debe crear
+    public static <T> void JsonArray_a_Repositorio (JSONArray jsonArray, Repositorio <T> repositorio, Class<? extends T> clase)
+    {
+        for(int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+            try {
+
+                Object objeto = clase.getDeclaredConstructor().newInstance();
+
+                if (objeto instanceof InterfazJson) {
+                    @SuppressWarnings("unchecked")
+                    InterfazJson<T> jObject = (InterfazJson<T>) objeto;
+
+                   //guardo en obj porque el constructor de fromJson devuelve un objeto armado
+                   T obj = jObject.fromJSON(jsonObject);
+                  //utilizo el metodo getID de la interface implementado en las clases, para pasarle al metodo agregar el id correspondiente de cada instancia
+                    repositorio.agregar(jObject.getID(),obj);
+                }else{
+                    System.out.println("La clase " + clase.getSimpleName() + "no implementa la InterfazJson");
+                }
+//agrego excepciones de la clase
+            } catch (JSONException | NoSuchMethodException | InstantiationException |
+                     IllegalAccessException | InvocationTargetException e ) {
+                System.out.println("Error creando el objeto" + clase.getSimpleName() + ": " + e.getMessage());
+            }
+        }
+    }
+
+
 }
